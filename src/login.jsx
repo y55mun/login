cb7b7dd7cf3207bdc147db19f5ec0e1e1a46862e
User <y55mun33@gmail.com>
2023-03-20 19:16:33 +0900

    login form

--- src/login.jsx
+++ src/login.jsx
@@ -1,8 +1,21 @@
+import React, { useState } from "react";    // 이메일과 비밀번호 입력을 캡쳐해옴
+
 export const Login = () => {
+  const [email, setEmail] = useState(''); // 이메일을 받은 다음 해당 상태를 수정하는 함수와 같은 이메일 후크 설정
+  const [pass, setPass] = useState('');
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.loge(email);
+}
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label for="email">email</label>
-      <input type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+      <label for="password">password</label>
+      <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
+      <button type="submit">Login</button>
     </form>
   )
 }
