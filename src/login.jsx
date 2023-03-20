9aeca37a7c9a82884bb15891bdc2fde2b304fddf
User <y55mun33@gmail.com>
2023-03-20 18:34:54 +0900

    FEAT: 이메일 기능 추가

---
+++ src/login.jsx
@@ -0,0 +1,8 @@
+export const Login = () => {
+  return (
+    <form>
+      <label for="email">email</label>
+      <input type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+    </form>
+  )
+}
