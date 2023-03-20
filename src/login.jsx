01ca8f154e2b5ebb2dc9c6f96eeddb880c9ceb39
y55mun <y55mun33@gmail.com>
2023-03-21 08:50:54 +0900

    focus 기능 추가: 비밀번호 미입력시 알림 창 띄우고 커서 위치도 비밀번호로 가게 수정

--- src/login.jsx
+++ src/login.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";    // 이메일과 비밀번호 입력을 캡쳐해옴
+import React, { useState, useRef } from "react";    // 이메일과 비밀번호 입력을 캡쳐해옴
 
 export const Login = () => {
   const [email, setEmail] = useState(''); // 이메일을 받은 다음 해당 상태를 수정하는 함수와 같은 이메일 후크 설정
   const [pass, setPass] = useState('');
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(email);
-}
+  const focusRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(pass === "") {
+      alert("비밀번호를 입력해주세요");
+      return focusRef.current.focus();
+    }
+  }
+
 
   return (
     <div className="auth-form-container">
@@ -15,7 +21,7 @@ const handleSubmit = (e) => {
         <label for="email">email</label>
         <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
         <label for="password">password</label>
-        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
+        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" ref={focusRef}/>
         <button className="link-btn" type="submit">Login</button>
       </form>
       
