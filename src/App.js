61a822d21991f8290aa07f9237e609dedec390d5
User <y55mun33@gmail.com>
2023-03-20 19:29:15 +0900

    design: change App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import { Login } from "./login";
 
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  
   return (
     <div className="App">
       <Login />
