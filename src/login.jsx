2c0cadc76bff74ff6c54949e391c5db737154bd0
User <y55mun33@gmail.com>
2023-03-20 19:30:13 +0900

    css

--- src/login.jsx
+++ src/login.jsx
@@ -10,12 +10,14 @@ const handleSubmit = (e) => {
 }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label for="email">email</label>
-      <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
-      <label for="password">password</label>
-      <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
-      <button type="submit">Login</button>
-    </form>
+    <div className="auth-form-container">
+      <form onSubmit={handleSubmit}>
+        <label for="email">email</label>
+        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+        <label for="password">password</label>
+        <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password"/>
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
